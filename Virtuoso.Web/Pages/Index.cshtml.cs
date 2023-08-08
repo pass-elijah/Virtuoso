258b3495ca9a47bd1a14bf38bf15d6dd63dd384c
pass-elijah <pass-elijah@hotmail.com>
2023-08-08 15:58:06 -0500

    Rename the web project

--- Virtuoso.Web/Pages/Index.cshtml.cs
+++ Virtuoso.Web/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Virtuoso.Pages
+namespace Virtuoso.Web.Pages
 {
     public class IndexModel : PageModel
     {
