7e87a3273f40e7941f924e195c31406558eee462
pass-elijah <pass-elijah@hotmail.com>
2023-08-08 18:24:00 -0500

    Add a Garment class. Maybe a normal linked list would work?

---
+++ src/Virtuoso.Domain/Garment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtuoso.Domain;
+
+public class Garment
+{
+    public string Name { get; set; } = string.Empty;
+
+    public Garment? DependsOn { get; set; }
+
+    public Garment? DependentOn { get; set; }
+}
