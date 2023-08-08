ed28f187360298b96698b408510a4a742ef22101
pass-elijah <pass-elijah@hotmail.com>
2023-08-08 18:58:20 -0500

    Get everything in to a dicrionary

--- src/Virtuoso.Web/Pages/Index.cshtml.cs
+++ src/Virtuoso.Web/Pages/Index.cshtml.cs
@@ -36,6 +36,23 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        var dict = new Dictionary<string, List<string>>();
 
+        // O(1) https://learn.microsoft.com/en-us/dotnet/api/system.array.getlength?view=net-7.0#remarks
+        var length = _example.GetLength(0);
+
+        for (int index = 0; index < length; index++)
+        {
+            var current = _example[index, 0];
+            var depend = _example[index, 1];
+            if (dict.TryGetValue(current, out _))
+            {
+                dict[current].Add(depend);
+            }
+            else
+            {
+                dict.Add(current, new() { depend });
+            }
+        }
     }
 }
