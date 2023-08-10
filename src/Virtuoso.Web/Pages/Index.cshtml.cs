e0315c9f79e17dc207538afbb978b56ff18079fc
pass-elijah <pass-elijah@hotmail.com>
2023-08-10 17:07:40 -0500

    Remove exmaple from Index

--- src/Virtuoso.Web/Pages/Index.cshtml.cs
+++ src/Virtuoso.Web/Pages/Index.cshtml.cs
@@ -12,28 +12,6 @@ public class IndexModel : PageModel
     private readonly ILogger<IndexModel> _logger;
     private readonly ITopologicalService _toposervice;
 
-    private readonly string[,] _example = new string[,]
-    {
-        {"t-shirt", "dress shirt"},
-        {"dress shirt", "pants"},
-        {"dress shirt", "suit jacket"},
-        {"tie", "suit jacket"},
-        {"pants", "suit jacket"},
-        {"belt", "suit jacket"},
-        {"suit jacket", "overcoat"},
-        {"dress shirt", "tie"},
-        {"suit jacket", "sun glasses"},
-        {"sun glasses", "overcoat"},
-        {"left sock", "pants"},
-        {"pants", "belt"},
-        {"suit jacket", "left shoe"},
-        {"suit jacket", "right shoe"},
-        {"left shoe", "overcoat"},
-        {"right sock", "pants"},
-        {"right shoe", "overcoat"},
-        {"t-shirt", "suit jacket"}
-   };
-
     public List<string> DressingSteps { get; set; } = new();
 
     public IndexModel(ILogger<IndexModel> logger, ITopologicalService toposervice)
@@ -44,6 +22,5 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-        DressingSteps = _toposervice.SortAndStratify(_example);
     }
 }
