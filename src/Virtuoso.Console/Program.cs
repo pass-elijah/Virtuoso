e1496175134af1b1f6228423aa8cb6d582a79cbb
pass-elijah <pass-elijah@hotmail.com>
2023-08-10 18:00:31 -0500

    Take the input for the console program

--- src/Virtuoso.Console/Program.cs
+++ src/Virtuoso.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var lineRegularExpression = new Regex("^(.*), (.*)");
+
+var input = new List<List<string>>();
+
+using var fileStream = File.OpenRead("input.txt");
+using var streamReader = new StreamReader(fileStream);
+string line;
+while ((line = streamReader.ReadLine()) != null)
+{
+    var match = lineRegularExpression.Match(line);
+    if (match.Success)
+    {
+        // the whole group is 0, and the matches are from 1
+        input.Add(new List<string> { match.Groups[1].ToString(), match.Groups[2].ToString() });
+    }
+}
+
+string[][] arryed = input.Select(i => i.ToArray()).ToArray();
