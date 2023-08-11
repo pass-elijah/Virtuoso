ee4759471fe22e0132fc154365461be3d3238c42
pass-elijah <pass-elijah@hotmail.com>
2023-08-11 09:21:14 -0500

    Move the filesytem access to it's own infrasturucre project

--- src/Virtuoso.Console/Program.cs
+++ src/Virtuoso.Console/Program.cs
@@ -1,25 +1,10 @@
-﻿using System.Text.RegularExpressions;
-using Virtuoso.Domain.Services;
-
-var lineRegularExpression = new Regex("^(.*), (.*)");
-
-var input = new List<string[]>();
-
-using var fileStream = File.OpenRead("input.txt");
-using var streamReader = new StreamReader(fileStream);
-string line;
-
-while ((line = streamReader.ReadLine()) != null)
-{
-    var match = lineRegularExpression.Match(line);
-    if (match.Success)
-    {
-        // The whole group is 0, and the matches are from 1
-        input.Add(new string[2] { match.Groups[1].ToString(), match.Groups[2].ToString() });
-    }
-}
+﻿using Virtuoso.Domain.Services;
+using Virtuoso.Infrastructure.Services;
 
 var topoGraphService = new TopologicalService();
+var fileService = new FileService();
+
+var input = fileService.GetInput("input.txt");
 
 var dressingOrder = topoGraphService.SortAndStratify(input.ToArray());
 
---
+++ src/Virtuoso.Infrastructure/Services/FileService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Virtuoso.Domain.Services;
+
+namespace Virtuoso.Infrastructure.Services;
+
+public class FileService : IFileService
+{
+    public List<string[]> GetInput(string path)
+    {
+        var lineRegularExpression = new Regex("^(.*), (.*)");
+
+        var input = new List<string[]>();
+
+        using var fileStream = File.OpenRead("input.txt");
+        using var streamReader = new StreamReader(fileStream);
+        string line;
+
+        while ((line = streamReader.ReadLine()) != null)
+        {
+            var match = lineRegularExpression.Match(line);
+            if (match.Success)
+            {
+                // The whole group is 0, and the matches are from 1
+                input.Add(new string[2] { match.Groups[1].ToString(), match.Groups[2].ToString() });
+            }
+        }
+
+        return input;
+    }
+}
